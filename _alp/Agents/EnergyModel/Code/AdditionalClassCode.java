735596efb45a65e2b3047f2de74efa1798f7a11f
Ate Hempenius <ate@zenmo.com>
2025-03-11 16:01:08 +0100

    addition of delivery and feedin capacity to the I_EnergyData

--- _alp/Agents/EnergyModel/Code/AdditionalClassCode.java
+++ _alp/Agents/EnergyModel/Code/AdditionalClassCode.java
@@ -3,5 +3,7 @@ public J_RapidRunData getRapidRunData(){return v_rapidRunData;}
 public J_LiveData getLiveData(){return v_liveData;}
 public J_RapidRunData getPreviousRapidRunData(){return v_previousRunData;}
 public J_ActiveAssetData getActiveAssetData(){return v_activeAssetData;}
+public double getDeliveryCapacity_kW(){return f_getTopLevelGridCapacity_kW();}
+public double getFeedinCapacity_kW(){return f_getTopLevelGridCapacity_kW();}
 public boolean getDeliveryCapacityKnown(){return false;}
 public boolean getFeedinCapacityKnown(){return false;}
--- _alp/Classes/Class.I_energyData.java
+++ _alp/Classes/Class.I_energyData.java
@@ -4,6 +4,8 @@ public interface I_EnergyData{
     J_RapidRunData getPreviousRapidRunData();
     J_ActiveAssetData getActiveAssetData();
     OL_ResultScope getScope();
+    double getDeliveryCapacity_kW();
+    double getFeedinCapacity_kW();
     boolean getDeliveryCapacityKnown();
     boolean getFeedinCapacityKnown();
 }
