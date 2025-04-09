ce086276ec1c8fe8716a5f932d0a3f5ad8450acf
Ate Hempenius <ate@zenmo.com>
2025-04-09 16:58:44 +0200

    bugfix: adding rapidrun asset totals to rapid run data of coop at initialization of custom coop aswell!

--- _alp/Agents/EnergyCoop/Code/Functions.java
+++ _alp/Agents/EnergyCoop/Code/Functions.java
@@ -1267,6 +1267,7 @@ v_cumulativeIndividualSelfSufficiencyEnergy_fr = v_rapidRunData.getTotalEnergyCo
 
 double f_getTotalInstalledCapacityOfAssets()
 {/*ALCODESTART::1740480839774*/
+//Collect live totals
 v_liveAssetsMetaData.totalInstalledWindPower_kW = 0.0;
 v_liveAssetsMetaData.totalInstalledPVPower_kW = 0.0;
 v_liveAssetsMetaData.totalInstalledBatteryStorageCapacity_MWh = 0.0;
@@ -1286,6 +1287,29 @@ for(Agent a :  c_coopMembers ) { // Take 'behind the meter' production and consu
 		v_liveAssetsMetaData.totalInstalledWindPower_kW += EC.v_liveAssetsMetaData.totalInstalledWindPower_kW;
 		v_liveAssetsMetaData.totalInstalledPVPower_kW += EC.v_liveAssetsMetaData.totalInstalledPVPower_kW;
 		v_liveAssetsMetaData.totalInstalledBatteryStorageCapacity_MWh += EC.v_liveAssetsMetaData.totalInstalledBatteryStorageCapacity_MWh;
+	}
+}
+
+//Collect rapid run totals
+v_rapidRunData.assetsMetaData.totalInstalledWindPower_kW = 0.0;
+v_rapidRunData.assetsMetaData.totalInstalledPVPower_kW = 0.0;
+v_rapidRunData.assetsMetaData.totalInstalledBatteryStorageCapacity_MWh = 0.0;
+
+//Add all battery storage capacities of gc
+for(GridConnection GC : c_memberGridConnections){
+	v_rapidRunData.assetsMetaData.totalInstalledWindPower_kW += GC.v_rapidRunData.assetsMetaData.totalInstalledWindPower_kW;
+	v_rapidRunData.assetsMetaData.totalInstalledPVPower_kW += GC.v_rapidRunData.assetsMetaData.totalInstalledPVPower_kW;
+	v_rapidRunData.assetsMetaData.totalInstalledBatteryStorageCapacity_MWh += GC.v_rapidRunData.assetsMetaData.totalInstalledBatteryStorageCapacity_MWh;
+}
+
+//Do this also for the 'child' coops
+for(Agent a :  c_coopMembers ) { // Take 'behind the meter' production and consumption!
+	if (a instanceof EnergyCoop) {
+		EnergyCoop EC = (EnergyCoop)a;
+		EC.f_getTotalInstalledCapacityOfAssets();
+		v_rapidRunData.assetsMetaData.totalInstalledWindPower_kW += EC.v_rapidRunData.assetsMetaData.totalInstalledWindPower_kW;
+		v_rapidRunData.assetsMetaData.totalInstalledPVPower_kW += EC.v_rapidRunData.assetsMetaData.totalInstalledPVPower_kW;
+		v_rapidRunData.assetsMetaData.totalInstalledBatteryStorageCapacity_MWh += EC.v_rapidRunData.assetsMetaData.totalInstalledBatteryStorageCapacity_MWh;
 	}
 }
 /*ALCODEEND*/}
