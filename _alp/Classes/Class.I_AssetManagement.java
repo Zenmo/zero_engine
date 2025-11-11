bc4e8596bc3bc986c5df336c557f9d5244e88246
Ate Hempenius <ate@zenmo.com>
2025-11-11 11:48:06 +0100

    save load bugfix

--- _alp/Classes/Class.I_AssetManagement.java
+++ _alp/Classes/Class.I_AssetManagement.java
@@ -1,5 +1,17 @@
 //This class contains all asset management classes are required to have.
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+@JsonIdentityInfo(generator = ObjectIdGenerators.UUIDGenerator.class, property = "@id")
+
+@JsonTypeInfo(
+	    use = JsonTypeInfo.Id.CLASS,
+	    include = JsonTypeInfo.As.PROPERTY,
+	    property = "type"  // 👈 this will be the field name in your JSON
+	)
+
 public interface I_AssetManagement
 {	
 	//Get agent that contains the management
