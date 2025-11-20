4eded71426c9a406c0d5d8a2db0ee922bfd6cff3
Ate Hempenius <ate@zenmo.com>
2025-11-20 11:51:09 +0100

    addition of J_HeatinfFunctionLibrary and J_GlobalParameters
    J_HeatinfFunctionLibrary  for now contains 2 hot water buffer management approaches (PT and default) default is not used yet by loader, but engine supports it.
    J_GlobalParameters class has been made that for now contains only the default floating point precision value.

---
+++ _alp/Classes/Class.J_GlobalParameters.java
@@ -0,0 +1,7 @@
+/**
+ * J_GlobalParameters
+ */	
+public abstract class J_GlobalParameters {
+	//public final static double floatingPointErrorMargin = 1e-15;
+	public final static int floatingPointPrecision = 15;
+}
