d8d5bb64985b942a706f4aaac78bf3e0f2fa4ac4
StijnPlekkenpol <s.h.plekkenpol@student.tue.nl>
2026-04-03 09:07:13 +0200

    HeatingManagementDistrictHeatingIronBurner

---
+++ _alp/Classes/Class.J_HeatingManagementDistrictHeatingIronBurner.java
@@ -0,0 +1,131 @@
+/**
+ * J_HeatingManagementDistrictHeatingIronBurner
+ */	
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+
+@JsonAutoDetect(
+    fieldVisibility = Visibility.ANY,    // 
+    getterVisibility = Visibility.NONE,
+    isGetterVisibility = Visibility.NONE,
+    setterVisibility = Visibility.NONE,
+    creatorVisibility = Visibility.NONE
+)
+public class J_HeatingManagementDistrictHeatingIronBurner implements I_HeatingManagement {
+
+	private boolean isInitialized = false;
+	private GridConnection gc;
+    private J_TimeParameters timeParameters;
+	private List<OL_GridConnectionHeatingType> validHeatingTypes = Arrays.asList(
+		OL_GridConnectionHeatingType.IRON_BURNER
+	);
+	private OL_GridConnectionHeatingType currentHeatingType;
+	private J_EAConversionIronBurner heatingAsset;
+	private J_HeatingPreferences heatingPreferences = null; //Not needed for the GCDistrictHeating.
+	
+	private double previousHeatFeedin_kW = 0;
+	
+	
+	//Stored
+	private double storedPreviousHeatFeedin_kW;
+	
+	/**
+     * Default constructor
+     */
+	public J_HeatingManagementDistrictHeatingIronBurner() {
+		
+	}
+	
+    public J_HeatingManagementDistrictHeatingIronBurner( GridConnection gc, J_TimeParameters timeParameters, OL_GridConnectionHeatingType heatingType ) {
+    	if (!(gc instanceof GCDistrictHeating)) {
+    		throw new RuntimeException("Impossible to connect " + this.getClass() + " to a GC that is not GCDistrictHeating");
+    	}
+    	this.gc = gc;
+    	this.timeParameters = timeParameters;
+    	this.currentHeatingType = heatingType;
+    }
+
+    public void manageHeating(J_TimeVariables timeVariables) {
+    	if ( !isInitialized ) {
+    		this.initializeAssets();
+    	}
+    	// Calculate current timestep demand by summing flows from all lower-level connections
+    	 
+    	double currentDemand_kW = 0;
+ 
+    	for (GridConnection childGC : gc.p_parentNodeHeat.f_getAllLowerLVLConnectedGridConnections()) {
+     	    if (childGC != gc) {
+     	        currentDemand_kW += childGC.fm_currentBalanceFlows_kW.get(OL_EnergyCarriers.HEAT);
+     	    }
+     	}
+ 
+    	double heatTransferToNetwork_kW = max(0, currentDemand_kW);
+    	gc.f_updateFlexAssetFlows(heatingAsset, heatTransferToNetwork_kW / heatingAsset.getOutputHeatCapacity_kW(), timeVariables);
+
+    	previousHeatFeedin_kW = -gc.fm_currentBalanceFlows_kW.get(OL_EnergyCarriers.HEAT);
+    }
+    
+    public void initializeAssets() {
+    	if (!validHeatingTypes.contains(this.currentHeatingType)) {
+    		throw new RuntimeException(this.getClass() + " does not support heating type: " + this.currentHeatingType);
+    	}
+    	if (gc.p_parentNodeHeat == null) {
+    		throw new RuntimeException(this.getClass() + " requires the GC: " + gc.p_gridConnectionID + " to be connected to a GridNodeHeat");
+    	}
+    	if (gc.p_heatBuffer != null) {
+    		throw new RuntimeException(this.getClass() + " does not support heat buffers.");
+    	}
+    	if (gc.p_BuildingThermalAsset != null) {
+    		throw new RuntimeException(this.getClass() + " does not support a building asset.");
+    	}
+    	if (gc.c_heatingAssets.size() == 0) {
+    		throw new RuntimeException(this.getClass() + " requires at least one heating asset.");
+    	}
+    	if (gc.c_heatingAssets.size() > 1) {
+    		throw new RuntimeException(this.getClass() + " does not support more than one heating asset.");
+    	}
+    	this.heatingAsset = (J_EAConversionIronBurner) gc.c_heatingAssets.get(0);
+    	this.isInitialized = true;
+    }
+    
+    public void notInitialized() {
+    	this.isInitialized = false;
+    }
+    
+    public List<OL_GridConnectionHeatingType> getValidHeatingTypes() {
+    	return this.validHeatingTypes;
+    }
+    
+    public OL_GridConnectionHeatingType getCurrentHeatingType() {
+    	return this.currentHeatingType;
+    }
+    
+    public void setHeatingPreferences(J_HeatingPreferences heatingPreferences) {
+    	this.heatingPreferences = heatingPreferences;
+    }
+    
+    public J_HeatingPreferences getHeatingPreferences() {
+    	return this.heatingPreferences;
+    }
+    
+   
+    //Get parentagent
+    public Agent getParentAgent() {
+    	return this.gc;
+    }
+    
+    
+    //Store and reset states
+	public void storeStatesAndReset() {
+		this.storedPreviousHeatFeedin_kW = this.previousHeatFeedin_kW;
+		this.previousHeatFeedin_kW = 0;
+	}
+	public void restoreStates() {
+		this.previousHeatFeedin_kW = this.storedPreviousHeatFeedin_kW;
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString();
+	}
+}
