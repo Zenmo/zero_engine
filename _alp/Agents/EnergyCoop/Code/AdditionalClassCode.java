4f33d8df33a70868b8a945eae60cadb6140c2413
Erik van Velzen <erik@evanv.nl>
2026-06-08 17:33:32 +0200

    Move code from onStartup to onCreate
    This makes it easier to programmatically create an energy model.
    
    I tested with the Woonwijk Demo model and I could not see any
    regressions.

--- _alp/Agents/EnergyCoop/Code/AdditionalClassCode.java
+++ _alp/Agents/EnergyCoop/Code/AdditionalClassCode.java
@@ -2,3 +2,16 @@ public OL_ResultScope getScope(){return OL_ResultScope.ENERGYCOOP;}
 public J_RapidRunData getRapidRunData(){return v_rapidRunData;}
 public J_LiveData getLiveData(){return v_liveData;}
 public J_RapidRunData getPreviousRapidRunData(){return v_previousRunData;}
+
+@Override
+public void onCreate() {
+    super.onCreate();
+    
+    v_liveData = new J_LiveData();
+    v_liveData.activeEnergyCarriers = EnumSet.of(OL_EnergyCarriers.ELECTRICITY);
+    v_liveData.activeProductionEnergyCarriers = EnumSet.of(OL_EnergyCarriers.ELECTRICITY);
+    v_liveData.activeConsumptionEnergyCarriers= EnumSet.of(OL_EnergyCarriers.ELECTRICITY);
+    v_liveData.connectionMetaData = v_liveConnectionMetaData;
+    v_liveData.assetsMetaData = v_liveAssetsMetaData;
+    energyModel.c_actors.add(this);
+}
